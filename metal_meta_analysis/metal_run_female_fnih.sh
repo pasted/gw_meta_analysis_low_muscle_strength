a0d5acacc8be02a859e6224da192f393d2c158f4
Garan Jones <garanjones@hotmail.com>
2020-11-17 03:21:21 +0000

    metal meta-analysis
    Initial commit - METAL scripts

---
+++ metal_meta_analysis/metal_run_female_fnih.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PBS -V   # export all environment variables to the batch job
+#PBS -d . # set working directory
+#PBS -A Research_Project-MRC158833
+#PBS -q sq # change to mrchq for high-memory queue / mrcq for mrc queue
+#PBS -l walltime=90:00:00,ncpus=1,mem=10G
+
+
+/gpfs/mrc0/projects/Research_Project-MRC158833/programs/metal/metal < _syntax.2020-03-13.gc.female.fnih.METAL.se.txt
